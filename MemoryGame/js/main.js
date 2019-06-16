d3ff5ec5ef7d0505f4fcb799c70b6d04121b4c05
Anthony <user@Users-MBP.attlocal.net>
2019-06-16 13:18:31 -0700

    created game logic

--- MemoryGame/js/main.js
+++ MemoryGame/js/main.js
@@ -1,35 +1,52 @@
 
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [ 
+{
+	rank: "queen",
+	suit: "heart",
+	cardImage: "images/queen-of-hearts.png"
+},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
 
 
 
 const cardsInPlay = [];
 	
-    if (cardsInPlay[0] === cardsInPlay[1]) 
-    {
-    	alert("You found a match!");
-    }
-	else {
-		alert("Sorry, try again.");
-}
+   
+
 
 var checkForMatch = function() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 };
 
 var flipcard = function() {
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardsId])
+	cardsInPlay.push(cards[cardsId].rank)
 
-}
+	console.log("User flipped" + cards[cardId].rank);
+	console.log(cards[cardsId].cardImage);
+	console.log(cards[cardsId].suit);
+};
 
-flippedCard(0);
-flippedCard(2);
 
 
 
