33b6fe5ff0a1dc5022e2bec292a1dd0f3d256efe
Anthony <user@Users-MBP.attlocal.net>
2019-06-15 22:11:20 -0700

    created game logic

--- MemoryGame/js/main.js
+++ MemoryGame/js/main.js
@@ -1,10 +1,33 @@
-console.log("Up and running!");
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
-console.log(" User flipped " + cardOne);
-console.log(" User flipped " + cardThree);
+var cardOne = cards [0];
+var cardTwo = cards [2];
+
+const cardsInPlay = []; 
+
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) 
+    {
+    	alert("You found a match!");
+    }
+	else {
+		alert("Sorry, try again.");
+}
+
+if (cardsInPlay.length === 2)
+
+{
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
+}
+
+{
+cardsInPlay.push("cardTwo");
+console.log("User flipped king");
+cardsInPlay;
+}
+
+ 
